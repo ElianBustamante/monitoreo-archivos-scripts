0ec1aaaedb839206f358d269da0c83b8572f3ef3
root <root@kali>
2023-11-28 00:11:54 -0500

    Se agregan modificaciones a script monitoreo archivo shadow.

--- verify-shadow-file.py
+++ verify-shadow-file.py
@@ -1,6 +1,8 @@
 import hashlib
 import os
 import smtplib
+import time
+import sys
 from email.message import EmailMessage
 
 def calcular_hash(archivo):
@@ -15,17 +17,21 @@ def enviar_correo():
     msg['Subject'] = 'ALERTA: El archivo SHADOW ha sido modificado'
     msg['From'] = 'elian.262000@gmail.com'
     msg['To'] = 'e.bustamante02@ufromail.cl'
-    s = smtplib.SMTP('smtp.gmail.com', 587)
-    s.starttls()
-    s.login('elian.262000@gmail.com', 'System.out.print("b747_$dreamliner");')
+    s = smtplib.SMTP('localhost')
     s.send_message(msg)
     s.quit()
 
 archivo = '/etc/shadow'
 hash_inicial = calcular_hash(archivo)
-
 while True:
     hash_actual = calcular_hash(archivo)
     if hash_actual != hash_inicial:
         enviar_correo()
-        hash_inicial = hash_actual
+        print('\n[Estado archivo SHADOW: MODIFICADO]')
+        print('*** [ALERTA] Archivo SHADOW ha sido modificado ***\n')
+    else: 
+        print('\n[Estado archivo SHADOW: CORRECTO]\n')
+
+    for i in range(10, 0, -1):
+        print(f"\rVerificando archivo SHADOW en: {i:<2}", end="")
+        time.sleep(1)
